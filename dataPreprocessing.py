8fce55b9ab2b10106fed0147612b2fbdbe1cca46
AoukshanaVChaphekar <chaphekaraoukshana@gmail.com>
2023-05-16 14:29:03 +0530

    Updated Data Processing

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -5,15 +5,17 @@ import os
 import cv2
 import pathlib
 import torch
+from torch.utils.data import DataLoader
 
 class DataProcessing():
 
-    def __init__(self, DATAPATH, source_lang, target_lang,device):
+    # Constructor for DataProcessing
+    def __init__(self, DATAPATH, source_lang, target_lang,device,config):
         
-        # Set source language
+        # Source language
         self.source_lang = source_lang
 
-        # Set target language
+        # Target language
         self.target_lang = target_lang
         
         # Start of Word and its Integer representation
@@ -33,8 +35,9 @@ class DataProcessing():
         self.UNK_char2int = 3 
 
         self.device = device
+        self.batch_size = config["batch_size"]
         
-        # Get path of train data,validation data and test data
+        # Get path of train,validation and test data
         self.trainPath = os.path.join(DATAPATH,self.target_lang,self.target_lang + "_train.csv")
         self.validationPath = os.path.join(DATAPATH,self.target_lang,self.target_lang + "_valid.csv")
         self.testPath = os.path.join(DATAPATH,self.target_lang,self.target_lang + "_test.csv")
@@ -68,37 +71,8 @@ class DataProcessing():
         
         self.source_char2int,self.source_int2char = self.source_vocab
         self.target_char2int,self.target_int2char = self.target_vocab
-  
-        # Create val data (only encode function suffices as the dictionary lookup should be kep the same)
-        # self.val_data = self.encode(
-        #     self.val["source"].to_list(),
-        #     self.val["target"].to_list(),
-        #     list(self.source_char2int.keys()),
-        #     list(self.target_char2int.keys()),
-        #     source_char2int=self.source_char2int,
-        #     target_char2int=self.target_char2int,
-        # )
-        
-        # # Store valEncoderInput,valDecoderInput,valDecoderTarget
-        # self.valEncoderInput, self.valDecoderInput, self.valDecoderTarget = self.val_data
-        # self.source_char2int,self.source_int2char = self.source_vocab
-        # self.target_char2int,self.target_int2char = self.target_vocab
-        
-        # # Create test data (only encode function suffices as the dictionary lookup should be kep the same)
-        # self.test_data = self.encode(
-        #     self.test["source"].to_list(),
-        #     self.test["target"].to_list(),
-        #     list(self.source_char2int.keys()),
-        #     list(self.target_char2int.keys()),
-        #     source_char2int=self.source_char2int,
-        #     target_char2int=self.target_char2int,
-        # )
-
-        # # Store testEncoderInput,testDecoderInput,testDecoderTarget
-        # self.testEncoderInput, self.testDecoderInput, self.testDecoderTarget = self.test_data
-        # self.source_char2int,self.source_int2char = self.source_vocab
-        # self.target_char2int,self.target_int2char = self.target_vocab
-
+        self.max_length = self.getMaxLength()
+       
     def encode(self, source_words, target_words, source_chars, target_chars, source_char2int = None, target_char2int = None):
         '''
         Input - 1.source_words - list of all source words
@@ -147,25 +121,10 @@ class DataProcessing():
             target_vocab = (target_char2int,target_int2char)
         
         
-        # TODO is this needed?
         self.encoder_input_data = np.zeros((len(source_words), self.max_source_length,self.num_encoder_tokens), dtype="float32")
         self.decoder_input_data = np.zeros((len(source_words), self.max_target_length,self.num_decoder_tokens), dtype="float32")
         self.decoder_target_data = np.zeros((len(source_words), self.max_target_length,self.num_decoder_tokens), dtype="float32")
             
-        # for i, (source_word, target_word) in enumerate(zip(source_words, target_words)):
-        #     for t, char in enumerate(source_word):
-        #         self.encoder_input_data[i, t,source_char2int[char]] = 1.0
-            
-        #     self.encoder_input_data[i, t + 1 :,source_char2int['.']] = 1.0 
-
-        #     for t, char in enumerate(target_word):
-        #         self. decoder_input_data[i, t,target_char2int[char]] = 1.0 
-        #         if t > 0:
-        #             self.decoder_target_data[i, t - 1, target_char2int[char]] = 1.0
-
-        #     self.decoder_input_data[i, t + 1 :,target_char2int['.']] = 1.0
-        #     self.decoder_target_data[i, t:, target_char2int['.']] = 1.0
-    
         
         if source_vocab != None and target_vocab != None:
             return (
@@ -183,23 +142,23 @@ class DataProcessing():
 
     def preprocess(self, source , target):
        
-        # creating list of words used in source language and converting them into string
+        # Creating list of words used in source language and converting them into string
         self.source_words = []
         for src in source:
             self.source_words.append(str(src))
         
-        # creating list of words used in target language and converting them into string
+        # Creating list of words used in target language and converting them into string
         self.target_words = []
         for trg in target:
             self.target_words.append(str(trg))
         
-        # set used to store characters used in source language
+        # Set used to store characters used in source language
         source_chars = set()
         
-        # set used to store characters used in target language
+        # Set used to store characters used in target language
         target_chars = set()
 
-        # populate source_chars and target)chars
+        # Populate source_chars and target_chars
         for src, tgt in zip(self.source_words, self.target_words):
             for char in src:
                 source_chars.add(char)
@@ -208,17 +167,18 @@ class DataProcessing():
                 target_chars.add(char)
 
         
+        # Total number of training samples
         self.number_of_train_samples = len(self.source_words)
         
         
-        # sort the characters used in source and target language       
+        # Sort the characters used in source and target language       
         source_chars = sorted(list(source_chars))
         target_chars = sorted(list(target_chars))
 
-        # Number of unique characters in source language
+        # Number of unique characters in source language + Start_of_word,end_of_word,padding and unknown token
         self.num_encoder_tokens = len(source_chars) + 4
         
-        # Number of unique characters in target language
+        # Number of unique characters in target language + Start_of_word,end_of_word,padding and unknown token
         self.num_decoder_tokens = len(target_chars) + 4
 
         # Length of maximum word in source_words
@@ -229,8 +189,8 @@ class DataProcessing():
 
         return self.encode(self.source_words, self.target_words, source_chars, target_chars)
 
+    # Returns list of integer mapped to character
     def indexesFromWord(self,lang,word):
-        # Returs list of character mapping to integer
         indexes = []
         if lang == "source":
             for char in word:
@@ -251,27 +211,26 @@ class DataProcessing():
 
     # Create tensor for word
     def tensorFromWord(self,lang, word):
-        # Gets list of character mapping to integer
+        # Gets list of integer mapped to character
         indexes = self.indexesFromWord(lang, word)
         
         # Append EOW2Int 
         indexes.append(self.EOW_char2int)
-
-        if lang == "source":
-            len_padding = self.max_source_length - len(indexes) + 1
-        if lang == "target":
-            len_padding = self.max_target_length - len(indexes) + 1
+        max_length = self.max_length
         
-        for i in range(len_padding):
-            indexes.append(self.PAD_char2int)
+        # Add padding
+        len_padding = max_length - len(indexes) + 1
+        
+        indexes.extend([self.PAD_char2int for i in range(len_padding)])
         
         return torch.tensor(indexes, dtype = torch.long, device = self.device).view(-1, 1)
 
     # Create tensor from pair 
-    def tensorsFromPair(self,pair):
+    def tensorsFromPair(self,pairs):
         # Get the source and target word for a given pair and generate tensors for them
-        source_tensor = self.tensorFromWord("source", pair[0])
-        target_tensor = self.tensorFromWord("target", pair[1])
+        source_tensor = self.tensorFromWord("source", pairs[0])
+        target_tensor = self.tensorFromWord("target", pairs[1])
+
         return (source_tensor, target_tensor)
     
     # Create list of pairs conataining source_words and target_words for train data
@@ -282,7 +241,6 @@ class DataProcessing():
 
         for source_word,target_word in zip(source_words,target_words):
             pairs.append((source_word,target_word))
-
         return pairs
     
     # Create list of pairs conataining source_words and target_words for validation data
@@ -319,5 +277,55 @@ class DataProcessing():
     
     # Returns the maximum length of word from source_lang and target_lang
     def getMaxLength(self):
-        return max(self.max_source_length,self.max_target_length)
+        
+        source_words = []
+        target_words = []
+        for word in self.val["source"].to_list():
+            source_words.append(word)
+
+        for word in self.val["target"].to_list():
+            target_words.append(word)
+        
+        val_max_source_length = max([len(txt) for txt in source_words])
+        val_max_target_length = max([len(txt) for txt in target_words])
+        
+        source_words = []
+        target_words = []
+        for word in self.test["source"].to_list():
+            source_words.append(word)
+
+        for word in self.test["target"].to_list():
+            target_words.append(word)
+        
+        test_max_source_length = max([len(txt) for txt in source_words])
+        test_max_target_length = max([len(txt) for txt in target_words])
+
+        self.maxSourceLength = max([self.max_source_length,val_max_source_length,test_max_source_length])
+        self.maxTargetLength = max([self.max_target_length,val_max_target_length,test_max_target_length])
+
+        return max(self.maxSourceLength,self.maxTargetLength)
+    
+    # Returns loader for validation data
+    def getValLoader(self):
+        
+        validationPairs = self.createValidationData()
+        validation_pairs = []
+        for pair in validationPairs:
+            validation_pairs.append(self.tensorsFromPair(pair))
+        
+        val_Loader = DataLoader(validation_pairs,batch_size = self.batch_size,shuffle = True)
+        return val_Loader
+    
+    # Returs loader for test data
+    def getTestLoader(self):
+        testPairs = self.createTestData()
+
+        test_pairs = []
+        for pair in testPairs:
+            test_pairs.append(self.tensorsFromPair(pair))
+        
+        test_Loader = DataLoader(test_pairs,batch_size = self.batch_size,shuffle = True)
+
+        return test_Loader
+    
     
